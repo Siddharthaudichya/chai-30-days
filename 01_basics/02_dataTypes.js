68c7398bceac18a32a02caac0b7d3f70e3f1538d
Siddharthaudichya <siddharthaudichya97@gmail.com>
2024-04-11 17:18:05 +0530

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -29,10 +29,67 @@ console.log(sage == mage);
 // Strictly equal to
 console.log(sage === mage);
 
+console.log("---------------");
+
+// bigInt definition
+let bigNum1 = BigInt(678136568658736587);
+//or
+let bigNum2 = 5873845738758989347n;    // Append 'n' at the end of the value
+
+console.log(bigNum1);
+console.log(bigNum2);
+
+console.log("---------------");
+
 // Define symbol
 let x = Symbol('prajwal');
 console.log(x.description);
 
+// checking if two variables with two data type symbol and same values are equal or not
+const id = Symbol("123");
+const anotherID = Symbol("123");
+
+console.log(id == anotherID);   //False
+
+console.log("---------------");
+
 // Object
 console.log(typeof null); // Object (IMP interview question)
 console.log(typeof undefined); // Undefined
+
+console.log("---------------");
+
+// Non-Primitive data types definition
+
+// Arrays
+let villiansNames = ["kirmada", "Mayandari", "Monjolika"];
+console.log(villiansNames[1]);
+
+// Objects
+let empInfo = {
+     name: "siddharth",
+     age: 21,
+     sex: "abhi tak Nahi kiya 21 saal ka ho gya"
+}
+
+console.log(empInfo.sex);
+
+console.log("---------------");
+
+// Functions
+let FirstName = "Siddharth";
+let LastName = "Audichya";
+
+// Method 1 : Defining it Separately
+function index1 (fName , lName) {
+    console.log(fName+ " " +lName);
+}
+
+index1 (FirstName, LastName);
+
+// Method 2 : Storing it in a variable
+let index2 = function(fName, lName) {
+    return fName+ " " + lName;
+}
+
+console.log(index2(FirstName, LastName));
---
+++ 01_basics/05_datatypesSummary.js
@@ -0,0 +1,8 @@
+// Lecture 9
+ 
+// Primitive data types : number, bigInt , string, boolean, null, undefined, symbol.
+// Non-primitive data types : Arrays, Functions, Objects.
+
+// Statically typed language: In these types of languages, data type of variables are decided during 'Compile time' eg : c,c++,java 
+// Dynamically typed language: In these types of languages, data type of variables are decided during 'Run time' eg : javascript, python, PhP
+
