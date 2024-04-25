2c068a1213d23fdb3a1009f2a9ac461d931bed72
kishlaykiku <kishlaykumarkkp24@gmail.com>
2024-04-25 20:46:45 +0530

    Control Flow

---
+++ 04_control_flow/01_if.js
@@ -0,0 +1,17 @@
+let score = 60;
+
+if(score <= 10) {
+    console.log("Nubda");
+} else if(score > 10 && score <= 50) {
+    console.log("Less Nubda");
+} else if(score > 50 && score <= 80) {
+    console.log("Pro");
+} else {
+    console.log("Ultra Pro Max");
+}
+
+
+// Ternary Oeprator
+let price = 80;
+
+price >= 80 ? console.log("Expensive") : console.log("Cheap");
